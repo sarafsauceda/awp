9f53e78b497b5106181f3f078a1af51643803f10
Sara Sauceda <sarafsauceda@gmail.com>
2022-02-11 11:09:18 -0800

    tier 3 in progress

--- server/api/robots.js
+++ server/api/robots.js
@@ -30,18 +30,22 @@ router.get('/:robotId', async (req, res, next) => {
   }
 });
 
-//POST /robots create new robot
+// POST /robots create new robot
 router.post('/', async (req, res, next) => {
-  const robotName = req.body.robotName
+  const name = req.body.name
   try {
-    const newRobot = await Robot.create({
-      robotName: robotName
-    })
+    const newRobot = await Robot.create(
+      { name: name }
+      )
+      console.log('new robot', newRobot)
   } catch (error) {
     next(error);
   }
 });
 
+// const newEmailRecord = await EBookSignup.create({ email: email });
+//         console.log(newEmailRecord);
+//         res.status(201).end(); // Successful signup!
 //need to add this in line 24? as: 'RobotProject'
 
 //  const robot = await Robot.findByPk(req.params.robotId, {
@@ -49,6 +53,7 @@ router.post('/', async (req, res, next) => {
 // })
 
 // res.status(201).send(await Robot.create(req.body));
-
+// const newRobot = await Robot.create({
+//   robotName: robotName
 module.exports = router;
 
